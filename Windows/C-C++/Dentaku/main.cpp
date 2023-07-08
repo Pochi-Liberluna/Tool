cc0da6f29bab1227249cb9c5f0646264de0be455
ぽち@Liberluna <121510825+Pochi-Liberluna@users.noreply.github.com>
2023-07-08 23:57:39 +0900

    Update main.cpp

--- Windows/C-C++/Dentaku/main.cpp
+++ Windows/C-C++/Dentaku/main.cpp
@@ -15,6 +15,41 @@ int main(void){
   std::cout << "" std::endl;
   std::cout << "" std::endl;
   std::cout << "" std::endl;
+
+  printf("これはコマンドプロンプト上で計算を行う簡易電子計算機です。¥nソースコードはGitHubに貼ってあります。->'https://github.com/Pochi-Liberluna/Tool/Windows/C-C++/Dentaku/'¥n");
+
+  printf("数値1を入力してください。=>");
+  scanf("%f",&sahen);
+
+  getchar()
+    
+  printf("¥n演算子を入力してください。=>");
+  scanf("%c",&enzan);
+
+  printf("¥n数値2を入力してください。=>");
+  scanf("%f",&uhen);
+
+  if(enzan == '+'){
+    answer = a + b;
+  } else if(enzan == '-'){
+    ansewr = a - b;
+  } else if(enzan == '*'){
+    answer = a * b;
+  } else if(enzan == '/'){
+    if(b != 0){
+    answer = a / b;
+  } else{
+      printf("0では割り算ができません。¥n");
+      return 0;
+  }
+} else{
+    printf("無効な演算子です。¥n");
+    return 0;
+}
+
+  printf("計算結果:%.2f¥n",answer); //少数第二位まで表示
+
+  return 0;
 }
 //AA描くのめんどい
 
