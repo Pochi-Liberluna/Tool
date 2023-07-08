588c4e49fdbd4daef6f05a62575da23065882252
ぽち@Liberluna <121510825+Pochi-Liberluna@users.noreply.github.com>
2023-07-08 11:48:33 +0900

    Delete C&C++ directory

--- C&C++/hello.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main(void){
-  printf("HelloWorld!¥n");
-
-  return 0;
-}
--- C&C++/hello.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-
-using namespace std;
-int main(void){
-  std::cout << "HelloWorld!¥n" << end|;
-
-  return 0;
-}
