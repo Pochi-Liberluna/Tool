a34c736249cc175da99c473e6d3eb6572c1a1c7f
ぽち@Liberluna <121510825+Pochi-Liberluna@users.noreply.github.com>
2023-07-08 22:50:21 +0900

    Create main.cpp

---
+++ Windows/C-C++/Dentaku/main.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+
+using namespace std;
+
+int main(void){
+  float sahen,uhen,answer;
+  char enzan;
+
+  std::cout << "¥n________  _________ ___    __ ___________ ___________ ___  __ __       __¥n" std::endl;
+  std::cout << "" std::endl;
+  std::cout << "" std::endl;
+  std::cout << "" std::endl;
+  std::cout << "" std::endl;
+  std::cout << "" std::endl;
+  std::cout << "" std::endl;
+}
