3a32acfcf5e0d9ef306b0f90029688a47f8a1cce
ぽち@Liberluna <121510825+Pochi-Liberluna@users.noreply.github.com>
2023-07-15 22:26:35 +0900

    Update BruteforceDictionary.py

--- Hack/Python/Bruteforce/BruteforceDictionary.py
+++ Hack/Python/Bruteforce/BruteforceDictionary.py
@@ -1,13 +1,20 @@
 import random
 from itertools import permutations
+import time
 
-characters = ["", "", "", "", "", "", "", "", "", "", ""]   #これは11文字に対応してますが自分で文字数を指定できます。
+characters = ["0", "1", "2", "3"]    #0,1,2,3を総当たり
 count = 0
 
-for perm in permutations(characters):
-    shuffled_word = ''.join(perm)
-    print(shuffled_word)
-    count += 1
+if not characters:
+    print("エラー: 総当たりする文字列が設定されていません。")
+else:
+    start_time = time.time()
 
-print("出力できた単語数:", count, "語")
-input("終了するには Enter キーを押してください...")
+    for perm in permutations(characters):
+        shuffled_word = ''.join(perm)
+        print(shuffled_word)
+        count += 1
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print("出力できた単語数:", count, "語", "{:.2f}秒".format(elapsed_time))
